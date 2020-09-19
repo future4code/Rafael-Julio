ba4e413f01137ffd5ba2634148dab7b194bbb399
rjuliuz <rjuliocoach@gmail.com>
2020-09-19 17:00:30 -0300

    Finalização do jogo blackjack na versão simples

---
+++ quinzena3/Blackjack/nossoJogo.js
@@ -0,0 +1,50 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+console.log("Bem vindo ao jogo de Blackjack.");
+ if(confirm("Quer iniciar uma nova rodada?")) {
+ // o que fazer se o usuário clicar "ok"
+
+ //adicionando 2 cartas ao usuário e 2 ao computador
+ const carta1Usuario = comprarCarta()
+ const carta2Usuario = comprarCarta()
+ const carta1Computador = comprarCarta()
+ const carta2Computador = comprarCarta()
+ 
+ // Somando os pontos das duas cartas.
+ let pontosUsuario = carta1Usuario.valor + carta2Usuario.valor
+ let pontosComputador = carta1Computador.valor + carta2Computador.valor
+ 
+ //Imprimindo no console as duas cartas + pontuação.
+ console.log("Usuário - cartas: "+carta1Usuario.texto+" "+carta2Usuario.texto+" - pontuação: "+ pontosUsuario)
+ console.log("Computador - cartas: "+carta1Computador.texto+" "+carta2Computador.texto+" - pontuação: "+ pontosComputador)
+ 
+ //Verificando quem ganhou:
+ if (pontosUsuario>pontosComputador) {
+    console.log("O usuário Ganhou!")
+ } else {
+    if (pontosComputador>pontosUsuario) {
+       console.log("O computador Ganhou!")
+
+      }  else {
+          console.log("Ihhhh, deu empate!")
+         }
+ }
+
+   } else {
+	   console.log("O jogo acabou");
+}
+
+
+
+ 
