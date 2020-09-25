a51814f20ac217916c83f6c0154716bfa9f3299b
rjuliuz <rjuliocoach@gmail.com>
2020-09-24 23:24:47 -0300

    finalização do projeto blackjack, inclusive com o desafio

--- quinzena3/Blackjack/desafio.js
+++ quinzena3/Blackjack/desafio.js
@@ -11,3 +11,57 @@
  * 
  */
 
+console.log("Bem vindo ao jogo de Blackjack.");
+ if(confirm("Quer iniciar uma nova rodada?")) {
+    //comprando as cartas do Player e do PC.
+   const cartasPlayer = [comprarCarta(),comprarCarta()]
+   const cartasPc = [comprarCarta(),comprarCarta()]
+
+   //Primeira regra, as duas cartas não pode ser AA. 
+   if (cartasPlayer[0].valor === 11 && cartasPlayer[1].valor === 11) {
+      cartasPlayer[0] = comprarCarta()
+      cartasPlayer[1] = comprarCarta()
+   } else if (cartasPc[0].valor === 11 && cartasPc[1].valor === 11) {
+      cartasPc[0] = comprarCarta()
+      cartasPc[1] = comprarCarta()
+   }
+   
+   let cartasTextosUser = [cartasPlayer[0].texto,cartasPlayer[1].texto]
+   let pontosPlayer = cartasPlayer[0].valor + cartasPlayer[1].valor
+   while( pontosPlayer < 21 && confirm(`Suas cartas são: ${cartasTextosUser}. A carta revelada do computador é ${cartasPc[0].texto}. Deseja comprar mais uma carta?`))   {
+      cartasPlayer.push(comprarCarta())
+      let i = 2
+      cartasTextosUser.push(cartasPlayer[i].texto)
+      pontosPlayer = pontosPlayer + cartasPlayer[i].valor
+      i++
+               
+   }
+
+   let cartasTextosPc = [cartasPc[0].texto, cartasPc[1].texto]
+   let pontosComputador = cartasPc[0].valor + cartasPc[1].valor
+
+   if (pontosPlayer > 21) {
+      alert(`Suas cartas são: ${cartasTextosUser}. Sua pontuação é ${pontosPlayer}. As cartas do computador são ${cartasTextosPc}. A pontuação do computador é ${pontosComputador}. O computador ganhou!`)
+   } else {
+      
+      while (pontosComputador < 21 && pontosComputador <= pontosPlayer) {
+         cartasPc.push(comprarCarta())
+         let e = 2 
+         cartasTextosPc.push(cartasPc[e].texto)
+         pontosComputador = pontosComputador + cartasPc[e].valor
+         e++
+      }
+   
+      if (pontosComputador > 21) {
+         alert(`Suas cartas são: ${cartasTextosUser}. Sua pontuação é ${pontosPlayer}. As cartas do computador são ${cartasTextosPc}. A pontuação do computador é ${pontosComputador}. Você ganhou, parabéns chuchu!`)
+      } else if (pontosComputador > pontosPlayer){
+         alert(`Suas cartas são: ${cartasTextosUser}. Sua pontuação é ${pontosPlayer}. As cartas do computador são ${cartasTextosPc}. A pontuação do computador é ${pontosComputador}. Não foi dessa vez bebe, o Computador ganhou!`)
+      } else {
+   
+      }
+   }
+
+} else {
+   console.log("O jogo acabou");
+}
+
--- quinzena3/Blackjack/nossoJogo.js
+++ quinzena3/Blackjack/nossoJogo.js
@@ -11,39 +11,39 @@
  * 
  */
 
-console.log("Bem vindo ao jogo de Blackjack.");
- if(confirm("Quer iniciar uma nova rodada?")) {
- // o que fazer se o usuário clicar "ok"
+// console.log("Bem vindo ao jogo de Blackjack.");
+//  if(confirm("Quer iniciar uma nova rodada?")) {
+//  // o que fazer se o usuário clicar "ok"
 
- //adicionando 2 cartas ao usuário e 2 ao computador
- const carta1Usuario = comprarCarta()
- const carta2Usuario = comprarCarta()
- const carta1Computador = comprarCarta()
- const carta2Computador = comprarCarta()
+//  //adicionando 2 cartas ao usuário e 2 ao computador
+//  const carta1Usuario = comprarCarta()
+//  const carta2Usuario = comprarCarta()
+//  const carta1Computador = comprarCarta()
+//  const carta2Computador = comprarCarta()
  
- // Somando os pontos das duas cartas.
- let pontosUsuario = carta1Usuario.valor + carta2Usuario.valor
- let pontosComputador = carta1Computador.valor + carta2Computador.valor
+//  // Somando os pontos das duas cartas.
+//  let pontosUsuario = carta1Usuario.valor + carta2Usuario.valor
+//  let pontosComputador = carta1Computador.valor + carta2Computador.valor
  
- //Imprimindo no console as duas cartas + pontuação.
- console.log("Usuário - cartas: "+carta1Usuario.texto+" "+carta2Usuario.texto+" - pontuação: "+ pontosUsuario)
- console.log("Computador - cartas: "+carta1Computador.texto+" "+carta2Computador.texto+" - pontuação: "+ pontosComputador)
+//  //Imprimindo no console as duas cartas + pontuação.
+//  console.log("Usuário - cartas: "+carta1Usuario.texto+" "+carta2Usuario.texto+" - pontuação: "+ pontosUsuario)
+//  console.log("Computador - cartas: "+carta1Computador.texto+" "+carta2Computador.texto+" - pontuação: "+ pontosComputador)
  
- //Verificando quem ganhou:
- if (pontosUsuario>pontosComputador) {
-    console.log("O usuário Ganhou!")
- } else {
-    if (pontosComputador>pontosUsuario) {
-       console.log("O computador Ganhou!")
+//  //Verificando quem ganhou:
+//  if (pontosUsuario>pontosComputador) {
+//     console.log("O usuário Ganhou!")
+//  } else {
+//     if (pontosComputador>pontosUsuario) {
+//        console.log("O computador Ganhou!")
 
-      }  else {
-          console.log("Ihhhh, deu empate!")
-         }
- }
+//       }  else {
+//           console.log("Ihhhh, deu empate!")
+//          }
+//  }
 
-   } else {
-	   console.log("O jogo acabou");
-}
+//    } else {
+// 	   console.log("O jogo acabou");
+// }
 
 
 
