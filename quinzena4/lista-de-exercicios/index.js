695dcf536173770e4297c65fbde390adb20834ea
rjuliuz <rjuliocoach@gmail.com>
2020-10-10 19:29:49 -0300

    finalização do exercício 3 de funções de array

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -275,3 +275,33 @@
 //     }
 // })
 
+//Exercício 3.
+
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+//A)
+// const podeEntrar = pessoas.map(function(item){
+//     if (item.idade > 14 && item.idade < 60 && item.altura >= 1.5){
+//         return item
+//     } 
+// })
+
+// console.log(podeEntrar)
+
+//B)
+// const naoPodeEntrar = pessoas.map(function(item){
+//     if (item.idade < 14 || item.idade > 60 || item.altura <= 1.5){
+//         return item
+//     } 
+// })
+
+// console.log(naoPodeEntrar)
+
+
